1597a49a79361ae1354df7fe2e172cd829ecd34a
jason <jason@johnson-net.ch>
2013-05-09 22:45:24 +0200

    Added a tag so the NFA can be printed even if it has cycles

--- src/Text/ExpressionEngine/Types.hs
+++ src/Text/ExpressionEngine/Types.hs
@@ -5,19 +5,33 @@ module Text.ExpressionEngine.Types
 )
 where
 
-data StateS a = Char a (Maybe (StateS a)) | SplitS (Maybe (StateS a)) (Maybe (StateS a)) | MatchS
 
-post2NFA chs = post2NFA' chs []
+type Tag = Int  -- needed to print since the structure is cyclic
+
+data StateS a = Char Tag a (Maybe (StateS a)) | SplitS (Maybe (StateS a)) (Maybe (StateS a)) | MatchS
+
+instance Show a => Show (StateS a) where
+    show nfa = show' nfa []
+        where
+            show' st@(Char n a (Just s)) seen
+                | n `elem` seen = showCharStart st ++ "##"
+                | otherwise = showCharStart st ++ "(" ++ show' s (n : seen) ++ ")"
+            show' (SplitS (Just s1) (Just s2)) seen = "SplitS (" ++ show' s1 seen ++ ") (" ++ show' s2 seen ++ ")"
+            show' MatchS _ = "MatchS"
+            showCharStart (Char n a _) = "Char " ++ show n ++ " " ++ show a ++ " "
+
+post2NFA :: [Char] -> StateS Char
+post2NFA chs = post2NFA' 1 chs []
     where
-        post2NFA' (c:cs) stack = case c of
-            '.' -> post2NFA' cs $ doDot stack
-            '|' -> post2NFA' cs $ doOr stack
-            '?' -> post2NFA' cs $ doQ stack
-            '*' -> post2NFA' cs $ doStar stack
-            '+' -> post2NFA' cs $ doPlus stack
-            _ -> post2NFA' cs (Char c Nothing : stack)
-        post2NFA' [] [nfa] = patch nfa MatchS
-        post2NFA' _ _ = error "malformed"
+        post2NFA' n (c:cs) stack = case c of
+            '.' -> post2NFA' n cs $ doDot stack
+            '|' -> post2NFA' n cs $ doOr stack
+            '?' -> post2NFA' n cs $ doQ stack
+            '*' -> post2NFA' n cs $ doStar stack
+            '+' -> post2NFA' n cs $ doPlus stack
+            _ -> post2NFA' (n+1) cs (Char n c Nothing : stack)
+        post2NFA' _ [] [nfa] = patch nfa MatchS
+        post2NFA' _ _ _ = error "malformed"
         doDot (b:a:stack) = patch a b : stack
         doDot _ = error "malformed postfix"
         doOr (b:a:stack) = SplitS (Just a) (Just b) : stack
@@ -33,8 +47,8 @@ post2NFA chs = post2NFA' chs []
             s' = patch a s
             in s' : stack
         doPlus _ = error "malformed"
-        patch (Char c Nothing) s = Char c (Just s)
-        patch (Char c (Just s)) s' = Char c $ Just $ patch s s'
+        patch (Char n c Nothing) s = Char n c (Just s)
+        patch (Char n c (Just s)) s' = Char n c $ Just $ patch s s'
         patch (SplitS a b) s = SplitS (patch' a s) (patch' b s)
         patch MatchS _ = error "malformed postfix"
         patch' Nothing s = Just s
