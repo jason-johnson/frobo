61e6e91d93205171dd53e1ce47071fca87ddcd92
jason <jason@johnson-net.ch>
2013-05-10 23:09:34 +0200

    NFA: top level module to export functions from Matcher and Parser

---
+++ src/Text/ExpressionEngine/NFA.hs
@@ -0,0 +1,11 @@
+module Text.ExpressionEngine.NFA
+(
+  match
+, match'
+, parseExpression
+, parseExpression'
+)
+where
+
+import Text.ExpressionEngine.NFA.Matcher
+import Text.ExpressionEngine.NFA.Parser
