be8b45bd7bf3049ffe29b4050477327acb057cd6
jason <jason@johnson-net.ch>
2013-05-22 16:13:08 +0200

    tests: updated NFA tests to all have anchor (comments)
    The previous schematics were that every expression has an implicit ^ at the start, so now that the parser works differently all the expressions needed to be updated to reflect this change.

--- testsuite/tests/Text/ExpressionEngine/NFATest.hs
+++ testsuite/tests/Text/ExpressionEngine/NFATest.hs
@@ -19,7 +19,7 @@ passes :: (b, (c, d, [State a])) -> Bool
 passes s = accept s || final s
 
 test_concat = do assertBool (accept $ match "abcdef" expr)
-    where expr = parseExpression "abcdef"
+    where expr = parseExpression "^abcdef"
 
 -- operators
 
@@ -27,7 +27,7 @@ test_or = do
     assertBool (accept $ match "abd" expr)
     assertBool (accept $ match "acd" expr)
     assertBool (not $ passes $ match "ad" expr)
-    where expr = parseExpression "a(b|c)d"
+    where expr = parseExpression "^a(b|c)d"
 
 test_star = do
     assertBool (accept $ match "d" expr)
@@ -35,7 +35,7 @@ test_star = do
     assertBool (accept $ match "aad" expr)
     assertBool (accept $ match "aaad" expr)
     assertBool (accept $ match "aaaaaaaaaaaaaaaad" expr)
-    where expr = parseExpression "a*d"
+    where expr = parseExpression "^a*d"
 
 test_plus = do
     assertBool (not $ passes $ match "d" expr)
@@ -43,27 +43,27 @@ test_plus = do
     assertBool (accept $ match "aad" expr)
     assertBool (accept $ match "aaad" expr)
     assertBool (accept $ match "aaaaaaaaaaaaaaaad" expr)
-    where expr = parseExpression "a+d"
+    where expr = parseExpression "^a+d"
 
 test_question = do
     assertBool (accept $ match "d" expr)
     assertBool (accept $ match "ad" expr)
     assertBool (not $ passes $ match "aad" expr)
-    where expr = parseExpression "a?d"
+    where expr = parseExpression "^a?d"
 
 test_noDollar = do
     assertBool (accept $ match "abc" expr)
     assertBool (accept $ match "abcd" expr)
     assertBool (accept $ match "abcdef" expr)
     assertBool (not $ passes $ match "abdc" expr)
-    where expr = parseExpression "abc"
+    where expr = parseExpression "^abc"
 
 test_dollar = do
     assertBool (final $ match "abc" expr)
     assertBool (not $ passes $ match "abcd" expr)
     assertBool (not $ passes $ match "abcdef" expr)
     assertBool (not $ passes $ match "abdc" expr)
-    where expr = parseExpression "abc$"
+    where expr = parseExpression "^abc$"
 
 test_group = do
     assertBool (accept $ match "abcd" expr)
@@ -71,7 +71,7 @@ test_group = do
     assertBool (not $ passes $ match "abyd" expr)
     assertBool (not $ passes $ match "axcd" expr)
     assertBool (not $ passes $ match "ad" expr)
-    where expr = parseExpression "a(bc|xy)d"
+    where expr = parseExpression "^a(bc|xy)d"
 
 test_groupStar = do
     assertBool (accept $ match "abcd" expr)
@@ -86,7 +86,7 @@ test_groupStar = do
     assertBool (not $ passes $ match "abyd" expr)
     assertBool (not $ passes $ match "abcbyd" expr)
     assertBool (not $ passes $ match "axcd" expr)
-    where expr = parseExpression "a(bc|xy)*d"
+    where expr = parseExpression "^a(bc|xy)*d"
     
 -- characters
 
@@ -95,17 +95,17 @@ test_dot = do
     assertBool (accept $ match "abc" expr)
     assertBool (accept $ match "a{c" expr)
     assertBool (not $ passes $ match "ac" expr)
-    where expr = parseExpression "a.c"
+    where expr = parseExpression "^a.c"
 
 test_leftCurly = do
     assertBool (accept $ match "a{c" expr)
     assertBool (not $ passes $ match "a{1c" expr)
-    where expr = parseExpression "a{c"
+    where expr = parseExpression "^a{c"
 
 test_escaped = do
     assertBool (accept $ match "a$c" expr)
     assertBool (not $ passes $ match "abc" expr)
-    where expr = parseExpression "a\\$c"
+    where expr = parseExpression "^a\\$c"
 
 test_range = do
     assertBool (accept $ match "aac" expr)
@@ -114,7 +114,7 @@ test_range = do
     assertBool (accept $ match "a]c" expr)
     assertBool (not $ passes $ match "ac" expr)
     assertBool (not $ passes $ match "adc" expr)
-    where expr = parseExpression "a[]ab-]c"
+    where expr = parseExpression "^a[]ab-]c"
 
 test_negRange = do
     assertBool (not $ passes $ match "aac" expr)
@@ -123,4 +123,4 @@ test_negRange = do
     assertBool (not $ passes $ match "ac" expr)
     assertBool (accept $ match "adc" expr)
     assertBool (accept $ match "a^c" expr)
-    where expr = parseExpression "a[^ab-]c"
+    where expr = parseExpression "^a[^ab-]c"
