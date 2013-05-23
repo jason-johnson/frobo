4dc2c36f164e8a48e69e009cb0f8cccab565686f
jason <jason@johnson-net.ch>
2013-05-23 20:29:22 +0200

    Matcher: reversed order of Monads to fix bogus group matching (comments)
    Ok, so previously we had (ListT (StateT)), which meant the list monad with a state that was visible to all traversals.  This is not what we actually wanted as this caused one traversal to record an OpenGroup that another traversal used even though it didn't see that specific one.  What we actually wanted was the state to be only visible to a given traversal.  So to achieve this we simply reverse the monads.  List is now the "inside" monad and State the outside.  This has the effect that each traversal has its own state for bookkeeping, so there is no longer a danger of a traversal seeing a CloseGroup and using the OpenGroup of a totally different traversal.
    
    NOTE: This does cause one bug, however.  Since the State is now only visible to a specific traversal, and not all of them, any matches that occur before the end of the string will be forgotten since match keeps running until the string is exhausted.  To fix this, we'll have to wrap what we have now around another state monad.  This new State will receive the "Results" data which will now have to include the final group results as well as the Accept state itself.

--- src/Text/ExpressionEngine/NFA/Matcher.hs
+++ src/Text/ExpressionEngine/NFA/Matcher.hs
@@ -6,11 +6,10 @@ module Text.ExpressionEngine.NFA.Matcher
 where
 
 import qualified Control.Monad.State as S
-import Control.Monad.List (ListT(..))
 import Text.ExpressionEngine.Types
 import Data.Set (member, notMember)
-import Data.Map (Map, (!))
-import qualified Data.Map as M (empty, insertWith)
+import Data.Map (Map)
+import qualified Data.Map as M (empty, insertWith, lookup)
 
 type Tag = Int
 
@@ -27,16 +26,16 @@ type GroupStart = (Start, [Start])
 type GroupStarts = Map Tag GroupStart
 
 -- NOTE: When we finalize this function, change all the aux match'' entries to just be match'.  They would have been match' but that creates a warning because of the benchmark match' we temporarily define
-match :: (Ord a, Show a) => [a] -> State a -> ([(Int, [Char])], (GroupStarts, GroupResults, Results a))
-match str ss = S.runState (runListT $ match'' (0 :: Int) str ss) (M.empty, M.empty, M.empty)
+match :: (Ord a, Show a) => [a] -> State a -> [([(Int, [Char])], (GroupStarts, GroupResults, Results a))]
+match str ss = S.runStateT (match'' (0 :: Int) str ss) (M.empty, M.empty, M.empty)
     where
-        match'' sc [] st@(Final _) = ListT $ recordWin st >> return [(sc, "match successful")]
-        match'' sc [] st@(Accept _) = ListT $ recordWin st >> return [(sc, "match successful")]
+        match'' sc [] st@(Final _) = recordWin st >> return [(sc, "match successful")]
+        match'' sc [] st@(Accept _) = recordWin st >> return [(sc, "match successful")]
         match'' sc cs st@(Split _ _) = toList st >>= match'' sc cs
         match'' sc cs (OpenGroup t s) = openGroup t sc >> match'' sc cs s
         match'' sc cs (CloseGroup t s) = closeGroup t sc >> match'' sc cs s
         match'' _ [] _ = failMatch
---        match'' sc [] thing = ListT . return $ [(sc, show thing)]
+--        match'' sc [] thing = return $ [(sc, show thing)]
         match'' _ _ (Final _) = failMatch
         match'' _ _ st@(Accept _) = recordWin st >> failMatch
         match'' sc (c:cs) (Step _ m s) = (if comp c m then toList s else failMatch) >>= match'' (sc+1) cs
@@ -44,19 +43,20 @@ match str ss = S.runState (runListT $ match'' (0 :: Int) str ss) (M.empty, M.emp
         comp _ Any = True
         comp c (OneOf s) = c `member` s
         comp c (NoneOf s) = c `notMember` s
-        toList st = ListT . return $ toList' st
+        toList = S.lift . toList'
         toList' (Split s1 s2) = toList' s1 ++ toList' s2
         toList' st = [st]
         openGroup t sc = S.modify $ (\(ogm, gm, rm) -> (openGroup' t sc ogm, gm, rm))
         openGroup' t sc ogm = M.insertWith (\_ (sc', scs) -> (min sc sc', sc:scs)) t (sc, [sc]) ogm
-        closeGroup t ec = S.modify $ (\(ogm, gm, rs) -> (ogm, closeGroup' t ec ogm gm, rs))
-        closeGroup' t ec ogm gm = M.insertWith (\_ (sc, ec', ecs) -> (sc, max ec ec', ec : ecs)) t (fst (ogm ! t), ec, [ec]) gm
+        closeGroup t ec = S.modify $ (\(ogm, gm, rs) -> (ogm, closeGroup' t ec (M.lookup t ogm) gm, rs))
+        closeGroup' t ec (Just og) gm = M.insertWith (\_ (sc, ec', ecs) -> (sc, max ec ec', ec : ecs)) t (fst og, ec, [ec]) gm
+        closeGroup' _ _ Nothing gm = gm
         recordWin st = S.modify $ (\(ogs, gs, rm) -> (ogs, gs, recordWin' st (resultTag st) rm))
         recordWin' st t rm = M.insertWith (\_ (_, c) -> (st, succ c)) t (st, 1) rm
         resultTag (Accept t) = t
         resultTag (Final t) = t
         resultTag _ = error "resultTag called on non-result"
-        failMatch = ListT . return $ []
+        failMatch = S.lift []
 
 match' :: Ord a => [a] -> State a -> [(Int, [Char])]
 match' str ss = match'' (0 :: Int) str ss
