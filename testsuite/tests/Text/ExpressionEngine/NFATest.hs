a33560057b1f77189525e2f02fc6e5ed9d4fa2b2
jason <jason@johnson-net.ch>
2013-05-25 18:28:08 +0200

    NFA parser: updated for new match function return type and made warning clean

--- testsuite/tests/Text/ExpressionEngine/NFATest.hs
+++ testsuite/tests/Text/ExpressionEngine/NFATest.hs
@@ -4,50 +4,62 @@ import Text.ExpressionEngine.NFA
 
 import Text.ExpressionEngine.Types
 import qualified Data.Map as M (lookup)
+import Data.Map (Map)
 
 import Test.Framework
 
-accept' :: Int -> Maybe ((State a), Int) -> Bool
-accept' 0 (Just (Accept _, n)) = n > 1
-accept' n (Just (Accept _, n')) = n == n'
+accept' :: (Num a, Ord a) => a -> Maybe (State t1, a, t) -> Bool
+accept' 0 (Just (Accept _, n, _)) = n > 1
+accept' n (Just (Accept _, n', _)) = n == n'
 accept' _ _ = False
 
+accept ::  (Num k, Num a, Ord k, Ord a) => (t, Map k (State t1, a, t2)) -> Bool
 accept (_,rm) = accept' 1 (M.lookup 1 rm)
-accept _ = False
+--accept _ = False
 
+accepts :: (Num k, Num a, Ord k, Ord a) => (t, Map k (State t1, a, t2)) -> Bool
 accepts (_,rm) = accept' 0 (M.lookup 1 rm)
-accepts _ = False
+--accepts _ = False
 
-final' :: Int -> Maybe ((State a), Int) -> Bool
-final' 0 (Just (Final _, n)) = n > 1
-final' n (Just (Final _, n')) = n == n'
+final' :: (Num a, Ord a) => a -> Maybe (State t1, a, t) -> Bool
+final' 0 (Just (Final _, n, _)) = n > 1
+final' n (Just (Final _, n', _)) = n == n'
 final' _ _ = False
 
+final :: (Num k, Num a, Ord k, Ord a) => (t, Map k (State t1, a, t2)) -> Bool
 final (_,rm) = final' 1 (M.lookup 1 rm)
-final _ = False
+--final _ = False
 
+finals :: (Num k, Num a, Ord k, Ord a) => (t, Map k (State t1, a, t2)) -> Bool
 finals (_,rm) = final' 0 (M.lookup 1 rm)
-finals _ = False
+--finals _ = False
 
+passes :: (Num a, Num k, Ord a, Ord k) => (t, Map k (State t1, a, t2)) -> Bool
 passes s = accept s || final s
 
+passess :: (Num a, Num k, Ord a, Ord k) => (t, Map k (State t1, a, t2)) -> Bool
 passess s = accepts s || finals s
 
+groupResult :: Ord k => k -> (t, (t1, Map k (t3, t4, t2))) -> Maybe (t3, t4)
 groupResult t (_, (_,gm)) = fmap (\(s,e,_) -> (s,e)) . M.lookup t $ gm
 
+groupMatch :: (Eq t, Eq t1, Ord k) => k -> (t, t1) -> ([(t2, (t3, Map k (t, t1, t4)))], b) -> Bool
 groupMatch t gr = any f . fst
     where
         f st = maybe False (gr ==) $ groupResult t st
 
+noGroupResult :: (Eq t4, Eq t3, Ord k) => k -> ([(t, (t1, Map k (t3, t4, t2)))], b) -> Bool
 noGroupResult t = all f . fst
     where
         f st = groupResult t st == Nothing
 
+test_concat :: IO ()
 test_concat = do assertBool (accept $ match "abcdef" expr)
     where expr = parseExpression "^abcdef"
 
 -- anchors
 
+test_noCarrot :: IO ()
 test_noCarrot = do
     assertBool (final $ match "ab" expr)
     assertBool (final $ match "aab" expr)
@@ -55,6 +67,7 @@ test_noCarrot = do
     assertBool (not $ passes $ match "abc" expr)
     where expr = parseExpression "ab$"
 
+test_carrot :: IO ()
 test_carrot = do
     assertBool (final $ match "ab" expr)
     assertBool (not $ passes $ match "aab" expr)
@@ -63,6 +76,7 @@ test_carrot = do
     where expr = parseExpression "^ab$"
 
 -- TODO: Make this test pass again.  It will require switching the mater to be:  StateT ListT State, with the inside state (visible to all traversals) receiving only the successes (probably with the group info that goes with it)
+test_noDollar :: IO ()
 test_noDollar = do
     assertBool (accept $ match "abc" expr)
     assertBool (accept $ match "abcd" expr)
@@ -70,6 +84,7 @@ test_noDollar = do
     assertBool (not $ passes $ match "abdc" expr)
     where expr = parseExpression "^abc"
 
+test_dollar :: IO ()
 test_dollar = do
     assertBool (final $ match "abc" expr)
     assertBool (not $ passes $ match "abcd" expr)
@@ -79,12 +94,14 @@ test_dollar = do
 
 -- operators
 
+test_or :: IO ()
 test_or = do
     assertBool (accept $ match "abd" expr)
     assertBool (accept $ match "acd" expr)
     assertBool (not $ passes $ match "ad" expr)
     where expr = parseExpression "^a(b|c)d"
 
+test_star :: IO ()
 test_star = do
     assertBool (accept $ match "d" expr)
     assertBool (accept $ match "ad" expr)
@@ -93,6 +110,7 @@ test_star = do
     assertBool (accept $ match "aaaaaaaaaaaaaaaad" expr)
     where expr = parseExpression "^a*d"
 
+test_plus :: IO ()
 test_plus = do
     assertBool (not $ passes $ match "d" expr)
     assertBool (accept $ match "ad" expr)
@@ -101,12 +119,14 @@ test_plus = do
     assertBool (accept $ match "aaaaaaaaaaaaaaaad" expr)
     where expr = parseExpression "^a+d"
 
+test_question :: IO ()
 test_question = do
     assertBool (accept $ match "d" expr)
     assertBool (accept $ match "ad" expr)
     assertBool (not $ passes $ match "aad" expr)
     where expr = parseExpression "^a?d"
 
+test_group :: IO ()
 test_group = do
     assertBool (accept $ match "abcd" expr)
     assertBool (accept $ match "axyd" expr)
@@ -115,6 +135,7 @@ test_group = do
     assertBool (not $ passes $ match "ad" expr)
     where expr = parseExpression "^a(bc|xy)d"
 
+test_groupStar :: IO ()
 test_groupStar = do
     assertBool (accept $ match "abcd" expr)
     assertBool (accept $ match "abcbcd" expr)
@@ -132,6 +153,7 @@ test_groupStar = do
     
 -- characters
 
+test_dot :: IO ()
 test_dot = do
     assertBool (accept $ match "aac" expr)
     assertBool (accept $ match "abc" expr)
@@ -139,16 +161,19 @@ test_dot = do
     assertBool (not $ passes $ match "ac" expr)
     where expr = parseExpression "^a.c"
 
+test_leftCurly :: IO ()
 test_leftCurly = do
     assertBool (accept $ match "a{c" expr)
     assertBool (not $ passes $ match "a{1c" expr)
     where expr = parseExpression "^a{c"
 
+test_escaped :: IO ()
 test_escaped = do
     assertBool (accept $ match "a$c" expr)
     assertBool (not $ passes $ match "abc" expr)
     where expr = parseExpression "^a\\$c"
 
+test_range :: IO ()
 test_range = do
     assertBool (accept $ match "aac" expr)
     assertBool (accept $ match "abc" expr)
@@ -158,6 +183,7 @@ test_range = do
     assertBool (not $ passes $ match "adc" expr)
     where expr = parseExpression "^a[]ab-]c"
 
+test_negRange :: IO ()
 test_negRange = do
     assertBool (not $ passes $ match "aac" expr)
     assertBool (not $ passes $ match "abc" expr)
@@ -169,6 +195,7 @@ test_negRange = do
 
 -- group capture
 
+test_starInside :: IO ()
 test_starInside = do
     assertBool (accept $ match "abd" expr)
     assertBool (accept $ match "abcd" expr)
@@ -177,6 +204,7 @@ test_starInside = do
     assertBool (not $ passes $ match "acd" expr)
     where expr = parseExpression "^a(bc*)d"
 
+test_starOutside :: IO ()
 test_starOutside = do
     assertBool (accept $ match "ad" expr)
     assertBool (accept $ match "abcd" expr)
@@ -185,6 +213,7 @@ test_starOutside = do
     assertBool (not $ passes $ match "acd" expr)
     where expr = parseExpression "^a(bc)*d"
 
+test_simpleCapture :: IO ()
 test_simpleCapture = do
     assertBool (groupMatch 1 (1,7) $ match "abcxybcd" expr)
     assertBool (groupMatch 1 (1,5) $ match "abcxyd" expr)
@@ -192,6 +221,7 @@ test_simpleCapture = do
     assertBool (noGroupResult 1 $ match "ad" expr)
     where expr = parseExpression "^a(bc|xy)*d"
 
+test_lateCapture :: IO ()
 test_lateCapture = do
     assertBool (groupMatch 1 (0,2) $ match "aee" expr)
     assertBool (groupMatch 1 (2,4) $ match "aebee" expr)
@@ -200,6 +230,7 @@ test_lateCapture = do
     assertBool (noGroupResult 1 $ match "be" expr)
     where expr = parseExpression "(ae|be)e$"
 
+test_longCapture :: IO ()
 test_longCapture = do
     assertBool (groupMatch 1 (1,2) $ match "aaa" expr)
     assertBool (groupMatch 1 (1,3) $ match "aaaa" expr)
